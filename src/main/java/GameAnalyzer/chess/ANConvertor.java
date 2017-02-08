b1878a41e06a4da47964f85c2a4b2f7d875f226a
Rohit <rohitvvv@gmail.com>
2017-02-08 23:07:41 +0530

    Added ANConverter Utility and UT for same

--- src/main/java/GameAnalyzer/chess/ANConvertor.java
+++ src/main/java/GameAnalyzer/chess/ANConvertor.java
@@ -1,5 +1,7 @@
 package GameAnalyzer.chess;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javafx.util.Pair;
 
 /**
@@ -8,13 +10,38 @@ import javafx.util.Pair;
  * @author Rohit_Vaidya
  */
  public final class ANConvertor {
+	  static Logger logger = LoggerFactory.getLogger(ANConvertor.class.getName());
+	  enum File{
+	      A,B,C,D,E,F,G,H;
+		  static int getOridinal(char ch){
+		    for(File file: File.values()){
+		       if(file.toString().equalsIgnoreCase(String.valueOf(ch)))
+		    	   return file.ordinal();
+		    }
+			return -1;
+		  }
+	  }
 	 /**
 	  * Takes an algebraic notation and returns a Pair 
-	  * @param an
-	  * @return
+	  * @param an The chess algebraic notation
+	  * @return Pair<Integer,Integer>
+	  * 
+	  * 8 . . . . . . . . 
+	  * . . . . . . . . .
+	  * . . . . . . . . .
+	  * 3 . . . . . . . .
+	  * 2 . . . . . . . .
+	  * 1 . . . . . . . . 
+	  *   A B C D E F G H
 	  */
  	 public static Pair<Integer,Integer> getMatrix(String an){
-		
- 		 return null;
- 	 }
- }
+         String strippedNotation = an.substring(1,an.length());
+         Integer file = File.getOridinal(strippedNotation.charAt(0));
+         Integer rank = Character.getNumericValue(strippedNotation.charAt(1));
+         Pair<Integer,Integer> pair = new Pair<Integer,Integer>(file,rank-1);
+         return pair;
+     }
+  }
+ 		
+      
+ 
--- src/test/java/GameAnalyzer/ANConverterTest.java
+++ src/test/java/GameAnalyzer/ANConverterTest.java
@@ -7,14 +7,28 @@ import org.junit.jupiter.api.Test;
 
 import GameAnalyzer.chess.ANConvertor;
 import javafx.util.Pair;
-public class ANConverterTest{
-    @Test
-	public void testANConvertor(){
-        final Pair<Integer,Integer> pair = new Pair<Integer, Integer>(0, 0);;
-        assertAll("Assert",
-        		() -> assertEquals(pair, ANConvertor.getMatrix("Ra1")),
-                () -> assertEquals(pair, ANConvertor.getMatrix("RA1")),
-                () -> assertEquals(pair, ANConvertor.getMatrix("ra1")));
-    	assertEquals(pair,ANConvertor.getMatrix("Ra1"));
+
+public class ANConverterTest {
+	@Test
+	public void testANConvertor() {
+		final Pair<Integer, Integer> pair1 = new Pair<Integer, Integer>(0, 0);
+		assertAll("Assert A File",
+				() -> assertEquals(pair1, ANConvertor.getMatrix("Ra1")),
+				() -> assertEquals(pair1, ANConvertor.getMatrix("RA1")),
+				() -> assertEquals(pair1, ANConvertor.getMatrix("ra1")));
+
+		final Pair<Integer, Integer> pair2 = new Pair<Integer, Integer>(7, 4);
+
+		assertAll("Assert H File",
+				() -> assertEquals(pair2, ANConvertor.getMatrix("kH5")),
+				() -> assertEquals(pair2, ANConvertor.getMatrix("qh5")),
+				() -> assertEquals(pair2, ANConvertor.getMatrix("rh5")));
+
+		final Pair<Integer, Integer> pair3 = new Pair<Integer, Integer>(2, 2);
+
+		assertAll("Assert C File", 
+				() -> assertEquals(pair3, ANConvertor.getMatrix("kc3")),
+				() -> assertEquals(pair3, ANConvertor.getMatrix("Kc3")),
+				() -> assertEquals(pair3, ANConvertor.getMatrix("qc3")));
 	}
 }
